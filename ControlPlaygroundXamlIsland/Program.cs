2cbaeb2488f22187693e96d3125a968e92466912
Michael Kreich <michaelkreich@hotmail.com>
2019-05-21 11:03:50 +0200

    cleanup

--- ControlPlaygroundXamlIsland/Program.cs
+++ ControlPlaygroundXamlIsland/Program.cs
@@ -1,14 +1,6 @@
 ﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
-using System.Windows.Media;
-using Windows.UI;
-using Windows.UI.Xaml.Media;
-using Color = Windows.UI.Color;
 using Colors = System.Windows.Media.Colors;
-using Rectangle = Windows.UI.Xaml.Shapes.Rectangle;
 using SW = System.Windows;
 
 namespace WpfApp
@@ -22,8 +14,6 @@ namespace WpfApp
             if (string.IsNullOrEmpty(AppDomain.CurrentDomain.GetData("FX_PRODUCT_VERSION") as string))
                 System.Diagnostics.Debug.Fail(".deps.json not deployed");
 #endif
-
-            //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
             using (var xamlApp = new XamlApp())
             {
--- ControlPlaygroundXamlIsland/XamlApp.cs
+++ ControlPlaygroundXamlIsland/XamlApp.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.Toolkit.Win32.UI.XamlHost;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using WUX = Windows.UI.Xaml;
 
 namespace WpfApp
@@ -11,9 +8,6 @@ namespace WpfApp
         public XamlApp()
         {
             MetadataProviders.Add(new ControlPlaygroundUwpLib.ControlPlaygroundUwpLib_XamlTypeInfo.XamlMetaDataProvider());
-            //Resources = new WUX.ResourceDictionary { Source = new Uri("ms-appx:///UwpLib/Styles/Styles.xaml") };
-            //Resources = new WUX.ResourceDictionary { Source = new Uri("ms-appx:///Magrathea.Presentation/Styles/MagratheaStyles.xaml") };
-
         }
     }
 }
